74a7d9815541ff1db77b423c5ab27d7e4e85def4
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-04 13:16:10 -0700

    Add basic functionality to generatePassword()

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generate the password
+function generatePassword() {
+  var length = prompt("Please enter the password length:");
+  if (length < 8 || length > 128) {
+    alert("Length must be between 8 and 128 characters!");
+    return;
+  }
+
+  var lower = confirm("Include lower case characters?");
+  var upper = confirm("Include upper case characters?");
+  var numbers = confirm("Include numbers?");
+  var specials = confirm("Include special characters?");
+
+  // Validate
+  if (lower == false && upper == false && numbers == false && specials == false) {
+    alert("You rejected all password criteria!\nUnable to generate password!");
+    return;
+  }
+  
+  return "swordfish";
+}
