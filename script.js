122d79107305d7349042f23248f476496cf046e7
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-04 13:33:25 -0700

    Add random generator code, sourced via StackOverflow

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add constants for each character type
+const upper_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower_chars = "abcdefghijklmnopqrstuvwxyz";
+const number_chars = "0123456789";
+const special_chars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; // I excluded spaces
 
 // Write password to the #password input
 function writePassword() {
@@ -12,12 +17,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Add constants for each character type
-const upper_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower_chars = "abcdefghijklmnopqrstuvwxyz";
-const number_chars = "0123456789";
-const special_chars = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; // I excluded spaces
 
 // generate the password
 function generatePassword() {
@@ -38,11 +37,28 @@ function generatePassword() {
     return;
   }
 
-  // random generation
-  // some functionality via https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
   var chosen_characters = ""; // these are the character types chosen via the confirms above
-
+  if (lower == true) {
+    chosen_characters += lower_chars;
+  }
+  if (upper == true) {
+    chosen_characters += upper_chars;
+  }
+  if (numbers == true) {
+    chosen_characters += number_chars;
+  }
+  if (specials == true) {
+    chosen_characters += special_chars;
+  }
   
+  // random generation
+  // functionality via https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  var result = "";
+  var ctr = 0;
+  while (ctr < length) {
+    result += chosen_characters.charAt(Math.floor(Math.random() * chosen_characters.length));
+    ctr += 1;
+  }
   
-  return "swordfish";
+  return result;
 }
